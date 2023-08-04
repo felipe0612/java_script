584b0e5aa5ca281a5c251d496dd02093edb5bc3c
felipe0612 <Felipe.bp1210@gmail.com>
2023-08-04 15:12:36 -0300

    ate_string
    Até as strings

---
+++ exemplos/2_TIPOS_DE_DADOS/4_string/script.js
@@ -0,0 +1,9 @@
+console.log("Meu nome é Marcos")
+console.log("Des4envolvimento é no senai")
+
+// Template litera (crase) ao lado da tecla P.
+// Usar Shift
+console.log(`teste`)
+
+console.log(typeof "Infinity")
+console.log(typeof Infinity)
